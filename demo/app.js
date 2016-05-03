4be300c3a9e61ff9bdf14c65430b6d904e5cfc5b
Cameron Gallarno <cgallarno@gmail.com>
2016-05-03 15:46:17 -0400

    Updated Froala

--- demo/app.js
+++ demo/app.js
@@ -1,34 +1,143 @@
 	// add the module with global defaults for froala
+	// Define popup template.
+	$.extend($.FroalaEditor.POPUP_TEMPLATES, {
+	  'customPlugin.popup': '[_BUTTONS_][_CUSTOM_LAYER_]'
+	});
+
+	// Define popup buttons.
+	$.extend($.FroalaEditor.DEFAULTS, {
+	  popupButtons: ['popupClose', '|', 'popupButton1', 'popupButton2'],
+	});
+
+	// The custom popup is defined inside a plugin (new or existing).
+	$.FroalaEditor.PLUGINS.customPlugin = function (editor) {
+	  // Create custom popup.
+	  function initPopup () {
+	    // Load popup template.
+	    var template = $.FroalaEditor.POPUP_TEMPLATES.customPopup;
+	    if (typeof template == 'function') template = template.apply(editor);
+
+	    // Popup buttons.
+	    var popup_buttons = '';
+
+	    // Create the list of buttons.
+	    if (editor.opts.popupButtons.length > 1) {
+	      popup_buttons += '<div class="fr-buttons">';
+	      popup_buttons += editor.button.buildList(editor.opts.popupButtons);
+	      popup_buttons += '</div>';
+	    }
+
+	    // Load popup template.
+	    var template = {
+	      buttons: popup_buttons,
+	      custom_layer: '<div class="custom-layer">Hello World!</div>'
+	    };
+
+	    // Create popup.
+	    var $popup = editor.popups.create('customPlugin.popup', template);
+
+	    return $popup;
+	  }
+
+	  // Show the popup
+	  function showPopup () {
+	    // Get the popup object defined above.
+	    var $popup = editor.popups.get('customPlugin.popup');
+
+	    // If popup doesn't exist then create it.
+	    // To improve performance it is best to create the popup when it is first needed
+	    // and not when the editor is initialized.
+	    if (!$popup) $popup = initPopup();
+
+	    // Set the editor toolbar as the popup's container.
+	    editor.popups.setContainer('customPlugin.popup', editor.$tb);
+
+	    // If the editor is not displayed when a toolbar button is pressed, then set BODY as the popup's container.
+	    // editor.popups.setContainer('customPlugin.popup', $('body'));
+
+	    // Trigger refresh for the popup.
+	    // editor.popups.refresh('customPlugin.popup');
+
+	    // This custom popup is opened by pressing a button from the editor's toolbar.
+	    // Get the button's object in order to place the popup relative to it.
+	    var $btn = editor.$tb.find('.fr-command[data-cmd="myButton"]');
+
+	    // Compute the popup's position.
+	    var left = $btn.offset().left + $btn.outerWidth() / 2;
+	    var top = $btn.offset().top + (editor.opts.toolbarBottom ? 10 : $btn.outerHeight() - 10);
+
+	    // Show the custom popup.
+	    // The button's outerHeight is required in case the popup needs to be displayed above it.
+	    editor.popups.show('customPlugin.popup', left, top, $btn.outerHeight());
+	  }
+
+	  // Hide the custom popup.
+	  function hidePopup () {
+	    editor.popups.hide('customPlugin.popup');
+	  }
+
+	  // Methods visible outside the plugin.
+	  return {
+	    showPopup: showPopup,
+	    hidePopup: hidePopup
+	  }
+	}
+
+	// Define an icon and command for the button that opens the custom popup.
+	$.FroalaEditor.DefineIcon('buttonIcon', { NAME: 'star'})
+	$.FroalaEditor.RegisterCommand('myButton', {
+	  title: 'Show Popup',
+	  icon: 'buttonIcon',
+	  undo: false,
+	  focus: false,
+	  plugin: 'customPlugin',
+	  callback: function () {
+	    this.customPlugin.showPopup();
+	  }
+	});
+
+	// Define custom popup close button icon and command.
+	$.FroalaEditor.DefineIcon('popupClose', { NAME: 'times' });
+	$.FroalaEditor.RegisterCommand('popupClose', {
+	  title: 'Close',
+	  undo: false,
+	  focus: false,
+	  callback: function () {
+	    this.customPlugin.hidePopup();
+	  }
+	});
+
+	// Define custom popup 1.
+	$.FroalaEditor.DefineIcon('popupButton1', { NAME: 'bell-o' });
+	$.FroalaEditor.RegisterCommand('popupButton1', {
+	  title: 'Button 1',
+	  undo: false,
+	  focus: false,
+	  callback: function () {
+	    alert("popupButton1 was pressed");
+	  }
+	});
+
+	// Define custom popup 2.
+	$.FroalaEditor.DefineIcon('popupButton2', { NAME: 'bullhorn' });
+	$.FroalaEditor.RegisterCommand('popupButton2', {
+	  title: 'Button 2',
+	  undo: false,
+	  focus: false,
+	  callback: function () {
+	    alert("popupButton2");
+	  }
+	});
+
 	var myApp = angular.module('myApp', ['froala']).
 	value('froalaConfig', {
 		toolbarInline: false,
-		placeholderText: 'Edit Your Content Here!'
+		placeholderText: 'Edit Your Content Here!',
+	//	toolbarButtons: ['myButton'],
 	});
 
 	// create the controller and inject Angular's $scope
 	myApp.controller('mainController', function($scope) {
-
-		// $scope.options = {
-		// 	placeholder : 'My Placeholder',
-		// 	onCtrlEnter : function(e){
-		// 		alert('Save it!');
-		// 	},
-		// 	onDelete : function(e){
-		// 		console.log('deleted');
-		// 	},
-		// 	events : {
-		// 		keydown : function(e){
-		// 			$scope.keyCode = e.keyCode;
-		// 			console.log('keydown', e.keyCode);
-		// 		}
-		// 	}
-		// };
-
-		// $scope.myHtml = '<p class="">Bacon ipsum dolor sit amet ball tip filet mignon frankfurter beef ribs tri-tip pork chop<dorsata-article-reference item-id="895" contenteditable="false"><span class="dorsata-article-reference-content" contenteditable="true" data-fr-verified="true">A Loop Region in the N-Terminal Domain of Ebola Virus VP40 Is Important in Viral Assembly, Budding, and Egress.</span></dorsata-article-reference> tail venison andouille pig spare ribs shankle. Kielbasa bacon sausage turducken pork loin. Sirloin hamburger prosciutto tongue turducken, chicken meatloaf ground round. Salami short loin pancetta&nbsp;tenderloin kevin rump.&nbsp;</p>';
-
-		// $scope.froalaAction = function(action){
-		// 	$scope.options.froala(action);
-		// }
 
 		$scope.titleOptions = {
 			placeholderText: 'Add a Title',
